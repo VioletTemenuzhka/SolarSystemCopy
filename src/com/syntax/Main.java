203647a51b66dcd608a23f6bd36f584ddd71753e
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-05-19 10:30:54 -0400

    Solar System Basic project Ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,13 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem Xfac = new SolarSystem();
+        Xfac.sun = "1";
+        Xfac.moon = "4";
+        Xfac.planet = "2";
+        Xfac.stars = "10000";
+    }
+}
---
+++ src/com/syntax/SolarSystem.java
@@ -0,0 +1,8 @@
+package com.syntax;
+
+public class SolarSystem {
+    String sun;
+    String moon;
+    String stars;
+    String planet;
+}
